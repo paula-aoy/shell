39cb9967f37234f092817a14f05361761a93c8a6
Paula Aoyagui <paula.aoyagui@mail.utoronto.ca>
2025-01-14 18:19:36 -0500

    correction rm/rf

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -40,11 +40,11 @@ cp "/Users/aoyaguip/downloads/DSI Shell/data/rawdata"/*server *.log "/Users/aoya
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm -riv */data/raw/*ipaddr
 rm -riv /data/user_logs/*ipaddr
-rf -rf ./data
+rt -rf ./data 
+corrected from coworker
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find "/Users/aoyaguip/downloads/DSI Shell/data/processed" -type f >> /"/Users/aoyaguip/downloads/DSI Shell/data/inventory.txt"
-
 
 ###########################################
 
