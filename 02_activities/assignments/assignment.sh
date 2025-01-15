b5046c6a7ed97efb727fee8be5035a444a9ab13c
Paula Aoyagui <paula.aoyagui@mail.utoronto.ca>
2025-01-14 20:55:15 -0500

    attempt 8:55

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -35,8 +35,8 @@ cp ./data/raw/*server*.log ./data/processed/server_logs
 cp ./data/raw/*server*.log ./data/processed/user_logs
 cp ./data/raw/*server*.log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm -riv /data/raw/*ipaddr*
-rm -riv /data/user_logs/*ipaddr*
+rm -riv ./data/raw/*ipaddr*
+rm -riv ./data/user_logs/*ipaddr*
 rt -rf ./data 
 #corrected from coworker
 
